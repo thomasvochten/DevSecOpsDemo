eca78cbaace489ae186386436002d30e111a0439
Thomas Vochten <mail@thomasvochten.com>
2023-03-23 13:20:45 +0100

    Introduced a new check

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Let's get this party started!");
+﻿Console.WriteLine("Let's get this party started!");
 
 // Deliberately hardcoded password
 var password = "pass@word1";
 Console.WriteLine("Password is: " + password);
+
+// Something silly
+var x = -1;
+if (x < 0)
+{
+  new ArgumentException("x must be nonnegative");
+}
+
+// TODO: Use an older nuget dependency to test Dependabot
+// TODO: Create more vulnerabilities to test Sonarcloud such as a weak hashing algorithm
