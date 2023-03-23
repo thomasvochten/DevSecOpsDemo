54c92b48970ed5215d0267e6041571e87abc3acf
Thomas Vochten <mail@thomasvochten.com>
2023-03-23 12:35:37 +0100

    🙈 Been doing PowerShell for way too long

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Let's get this party started!");
 
-# deliberately hardcoded password
+// Deliberately hardcoded password
 var password = "pass@word1";
-Console.WriteLine("Password is: " + password);                        
+Console.WriteLine("Password is: " + password);
