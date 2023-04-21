1e6c64b45b06e7c586a7fc0f1a262e050bc5d763
Thomas Vochten <mail@thomasvochten.com>
2023-04-21 15:55:38 +0200

    Don't try this at home

--- Program.cs
+++ Program.cs
@@ -25,13 +25,18 @@ class Program
             new ArgumentException("x must be nonnegative");
         }
 
+        
         const int MAX = 100;
         if (MAX == 100)
         {
             Console.WriteLine("MAX is exactly 100");
         }
 
+        // Deliberately hardcoded secrets
         BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=DevSecOpsDemo;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
+        
+        string sas_token = "sp=rl&st=2023-04-21T13:53:33Z&se=2024-04-21T21:53:33Z&spr=https&sv=2021-12-02&sr=c&sig=Wk7kp6JMRaHaxUtOa0nkBR2zSuFQd1JfnkV9md8TiJw%3D";
+        string sas_url = "https://thisivulnerablestorage.blob.core.windows.net/demo-devsecops?sp=rl&st=2023-04-21T13:53:33Z&se=2024-04-21T21:53:33Z&spr=https&sv=2021-12-02&sr=c&sig=Wk7kp6JMRaHaxUtOa0nkBR2zSuFQd1JfnkV9md8TiJw%3D";
 
         // TODO: Create more vulnerabilities to test Sonarcloud such as a weak hashing algorithm    }
     }
