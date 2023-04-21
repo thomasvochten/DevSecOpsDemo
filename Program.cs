0e7a41d538a838f02e3e10c9091b6fb17564ffe7
Thomas Vochten <mail@thomasvochten.com>
2023-04-21 16:14:34 +0200

    On a roll!

--- Program.cs
+++ Program.cs
@@ -33,10 +33,15 @@ class Program
         }
 
         // Deliberately hardcoded secrets
-        BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=DevSecOpsDemo;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
-        
+        BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=thisivulnerablestorage;AccountKey=+eAwigsvbNxLb7ZcOxvgrHjuXE0oliYfNQb21rpeuFkENfUoJ/abfDw1eOaSwIGOR1CIHWYGIXZx+AStJb4kqA==;EndpointSuffix=core.windows.net");
+        blobServiceClient.GetBlobContainerClient("demo-devsecops").GetBlobClient("demo-devsecops.txt").DownloadTo("demo-devsecops.txt");
+
+
         string sas_token = "sp=rl&st=2023-04-21T13:53:33Z&se=2024-04-21T21:53:33Z&spr=https&sv=2021-12-02&sr=c&sig=Wk7kp6JMRaHaxUtOa0nkBR2zSuFQd1JfnkV9md8TiJw%3D";
         string sas_url = "https://thisivulnerablestorage.blob.core.windows.net/demo-devsecops?sp=rl&st=2023-04-21T13:53:33Z&se=2024-04-21T21:53:33Z&spr=https&sv=2021-12-02&sr=c&sig=Wk7kp6JMRaHaxUtOa0nkBR2zSuFQd1JfnkV9md8TiJw%3D";
+
+        Console.WriteLine("SAS token is: " + sas_token);
+        Console.WriteLine("SAS URL is: " + sas_url);
 
         // TODO: Create more vulnerabilities to test Sonarcloud such as a weak hashing algorithm    }
     }
